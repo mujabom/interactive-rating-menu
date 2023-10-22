c5c6793a5fac8dd37efbfab2cd1e495051c62d56
mujabom <msaabwsr@gmail.com>
2023-10-22 20:34:12 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,20 @@ export default function App5() {
   const [rating, setRating] = useState<number>(0);
   return (
     <div className=" min-h-screen justify-center flex align-middle items-center bg-black">
-      <div className="bg-gradient-to-b flex flex-col from-gray-700 p-8 justify-center h-96 overflow-hidden to-gray-900 aspect-square rounded-3xl m-auto ">
+      <div className="bg-gradient-to-b flex flex-col justify-around from-gray-700 p-8 pt6  h-[26rem] overflow-hidden to-gray-900 aspect-square rounded-3xl m-auto ">
         {step === "step1" && (
           <>
-            <span className="mb-3 w-fit flex justify-center text-center items-center p-3 bg-gray-600 rounded-full">
-              <Star className="text-orange-400 text-9xl " />
+            <span className=" w-fit flex justify-center text-center items-center p-2 bg-gray-600 rounded-full">
+              <Star className="text-orange-500" />
             </span>
-            <h2 className="text-3xl mb-3 text-white font-bold">How did we do?</h2>
-            <p className=" text-gray-400 text-xs pr-3 mb-5">
+            <h2 className="text-3xl  text-white font-bold">How did we do?</h2>
+            <p className=" text-gray-400 text-sm pr-3">
               Please let us know how we did with your support request. All
               feedback is appreciated to help us improve our offering!
             </p>
-            <div className="flex flex-row py-3
-          
-        justify-around w-auto">
+            <div
+              className="flex flex-row py-3 justify-around w-auto"
+            >
               <RatingButton number={1} rating={rating} setRating={setRating} />
               <RatingButton number={2} rating={rating} setRating={setRating} />
               <RatingButton number={3} rating={rating} setRating={setRating} />
@@ -30,7 +30,7 @@ export default function App5() {
             </div>
             <button
               onClick={() => setStep("step2")}
-              className="bg-orange-400 mt-6 text-white w-full py-3 rounded-3xl  hover:bg-white hover:text-orange-400"
+              className="bg-orange-500 text-white w-full py-3 rounded-3xl  hover:bg-white hover:text-orange-500"
             >
               Submit
             </button>
@@ -59,7 +59,7 @@ function RatingButton({
           rating === number
             ? "text-white bg-slate-400"
             : "text-gray-300 flex bg-slate-600"
-        } text-center hover:bg-orange-400 hover:text-white justify-center p-3 rounded-full`}
+        } text-center hover:bg-orange-500 hover:text-white justify-center p-3 rounded-full`}
       >
         <span className="flex aspect-square w-6 text-center justify-center align-middle">
           {number}
@@ -76,7 +76,7 @@ function Result({ rating }: { rating: number }) {
         <img src={illthank} alt="thank you" className="" />
         <span
           className="my-6 text-center
-      text-orange-400 text-sm bg-slate-600 justify-center py-1 px-3 rounded-full"
+      text-orange-500 text-sm bg-slate-600 justify-center py-1 px-3 rounded-full"
         >
           <span>you selected {rating} out of 5</span>
         </span>
